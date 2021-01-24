97a2b6cb12d184bd9b167e7130376a298c10e95b
janossik <marcinczaniecki97@gmail.com>
2021-01-24 09:03:47 +0100

    Add styles and photo, updated textarea
    Updated textarea, now both can encrypt and decrypt

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,16 @@
 import { caesarCipher } from "./caesar-cipher";
 
 const inputMessage = document.getElementById("message");
-const outputMessage = document.getElementById("emcrypt_message");
+const outputMessage = document.getElementById("encrypted_message");
 
 inputMessage.addEventListener("keyup", (e) => {
-  outputMessage.value = caesarCipher(e.target.value);
+  if (e.target.value) {
+    outputMessage.value = caesarCipher(e.target.value);
+  }
+});
+
+outputMessage.addEventListener("keyup", (e) => {
+  if (e.target.value) {
+    inputMessage.value = caesarCipher(e.target.value);
+  }
 });
